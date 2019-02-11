92fd906014e7f6a57333630d512bc3e4acf6ca15
Soroosh Rezazadeh <soroosh.rezazadeh@gmail.com>
2019-02-11 14:58:24 -0500

    bug fix for the api update

--- Boltzmann_Machines/rbm.py
+++ Boltzmann_Machines/rbm.py
@@ -77,14 +77,13 @@ class RBM():
         p_v_given_h = torch.sigmoid(activation)  # P(v=1|h)
         return p_v_given_h, torch.bernoulli(p_v_given_h)
     
-    
     def train(self, v0, vk, ph0, phk):
-        self.W += torch.mm(v0.t(), ph0) - torch.mm(vk.t(), phk)
+        self.W += (torch.mm(v0.t(),ph0) - torch.mm(vk.t(),phk)).t()
         self.b += torch.sum((v0 - vk), 0)
         self.a += torch.sum((ph0 - phk), 0)
         
-nv = len(training_set[0])
-nh = 100
+nv = len(training_set[0]) # number of features in the training set
+nh = 100  # number of features we want to detect
 batch_size = 100
 rbm = RBM(nv, nh)
 
@@ -92,18 +91,21 @@ rbm = RBM(nv, nh)
 nb_epoch = 10
 for epoch in range(1, nb_epoch + 1):
     train_loss = 0
-    s = 0.
+    s = 0.  # counter for normalizing train_loss
     for id_user in range(0, nb_users - batch_size, batch_size):
-        vk = training_set[id_user:id_user+batch_size]
+        vk = training_set[id_user:id_user+batch_size] # the output after k steps of contrastive divergence
         v0 = training_set[id_user:id_user+batch_size]
         ph0,_ = rbm.sample_h(v0)
+
+        # K-step contrastive divergence
         for k in range(10):
             _,hk = rbm.sample_h(vk)
             _,vk = rbm.sample_v(hk)
             vk[v0<0] = v0[v0<0]
         phk,_ = rbm.sample_h(vk)
         rbm.train(v0, vk, ph0, phk)
-        train_loss += torch.mean(torch.abs(v0[v0>=0] - vk[v0>=0]))
+        train_loss += torch.mean(torch.abs(v0[v0>=0] - vk[v0>=0])) # Average Distance here
+        # train_loss += np.sqrt(torch.mean((v0[v0>=0] - vk[v0>=0])**2)) # RMSE here
         s += 1.
     print('epoch: '+str(epoch)+' loss: '+str(train_loss/s))
 
@@ -112,10 +114,11 @@ test_loss = 0
 s = 0.
 for id_user in range(nb_users):
     v = training_set[id_user:id_user+1]
-    vt = test_set[id_user:id_user+1]
+    vt = test_set[id_user:id_user+1]  # target v
     if len(vt[vt>=0]) > 0:
         _,h = rbm.sample_h(v)
         _,v = rbm.sample_v(h)
         test_loss += torch.mean(torch.abs(vt[vt>=0] - v[vt>=0]))
+        # test_loss += np.sqrt(torch.mean((vt[vt>=0] - v[vt>=0])**2)) # RMSE here
         s += 1.
 print('test loss: '+str(test_loss/s))
